fc2fa2722aca7a83e1ce42fa1614d1b9f7e09316
Kritikas8349 <kritikasingh8349@gmail.com>
2026-07-22 13:58:05 +0530

    added navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import Home from "./pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-
-import './App.css';
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
-        <Home />
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
