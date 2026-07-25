2a388400c96f75382bedf54bfd24e60f5fc1b92d
Kritikas8349 <kritikasingh8349@gmail.com>
2026-07-25 14:48:12 +0530

    size and padding fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <Footer />
